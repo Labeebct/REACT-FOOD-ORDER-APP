358effffb2398886a419b7027822364c287f02f7
Labeebct <ctlabeebthaliyil@gmail.com>
2024-03-17 22:25:33 +0530

    completed admin login

--- src/components/admin/AdminLoginContent.jsx
+++ src/components/admin/AdminLoginContent.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axiosInstance from '../../instance/axiosInstance.js'
+import { useNavigate } from 'react-router-dom'
 
 function AdminLoginContent() {
+
+  const Navigate = useNavigate()
+
+  //State to show the error messages
+  const [message , setMessage] = useState('')
+  const [success , setSuccess] = useState(false)
+
+
+  const handleSubmit = async(e) => {
+    try {
+
+      e.preventDefault()
+
+      //Regex for validating entering datas
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      //Destructuring datas from form target
+      const {email , password} = e.target
+
+      //Validating datas
+      if(!email.value && !password.value){
+        setMessage('Please Fill all Fields')
+      } else if (!email.value) {
+        setMessage('Please Enter the Email')
+      } else if (!password.value) {
+        setMessage('Please Enter the Password')
+      }  else if (!emailRegex.test(email.value)){
+        setMessage('Invalid Email Format')
+      } else {
+        try {
+
+          //Sending api reqeust to backend
+          const response = await axiosInstance.post('/admin/login',{
+            email:email.value,
+            password:password.value
+          })
+
+          //Destructuring data from response
+          const {data , status} = response
+          //Redirecting to admin dashboard if response is ok
+          if(status == 200){
+            setSuccess(true)
+            setMessage(data.msg)
+            setTimeout(() => Navigate('/admin/dashboard'), 800);
+          }
+        } catch (error) {
+          if(error.response){
+            //Destructuring data and status from error responce
+            const {data , status} = error.response
+
+            if(status == 422 || status == 401 || status == 404){
+               setMessage(data.msg)
+            } else {
+              console.log('Unexpected responce from the server',error);
+            }
+
+          } else {
+            console.log('No response from server',error);
+          }
+        }
+      }
+      
+    } catch (error) {
+      console.log('Error in login handle submit',error);
+    }
+  }
+
+  if(message) setTimeout(() =>setMessage(''), 2000);
+  
   return (
     <div className='w-full h-full flex items-center justify-center'>
      <section className="w-[33%] mb-2 xl:w-[29%] min-w-[320px] sm:min-w-[370px] h-[34rem] bg-[#fdfdfd14]  overflow-hidden rounded-[.6rem] flex justify-center">
-        <form className="w-full text-black h-full flex  flex-col items-center px-[2.6rem] py-9 gap-4 flex-1">
+        <form onSubmit={handleSubmit} className="w-full text-black h-full flex  flex-col items-center px-[2.6rem] py-9 gap-4 flex-1">
           <h4 className="text-[2.2rem] mt-2 font-playfair text-white">Login</h4>
           <div className="w-full h-auto flex flex-col mt-9 gap-2">
             <label
@@ -16,6 +87,7 @@ function AdminLoginContent() {
             <input
               spellCheck={false}
               type="text"
+              name='email'
               className="h-[2.5rem] border rounded-[3rem] outline-none px-5 text-[.9rem] border-[#39393948] drop-shadow-sm bg-[#ffffff6a] text-white"
             />
             <label
@@ -27,10 +99,12 @@ function AdminLoginContent() {
             <input
               spellCheck={false}
               type="password"
+              name='password'
+              maxLength={16}
               className="h-[2.5rem] border rounded-[3rem] outline-none px-5 text-[.9rem] border-[#39393948] drop-shadow-sm bg-[#ffffff6a] text-white"
             />
           </div>
-          <p className="text-red-500 text-center mt-6">Incorrect email</p>
+          <p className={`text-[.95rem] ${success ? 'text-green-600' : 'text-red-500' } text-center mt-6`}>{message}</p>
           <button className="px-8 py-2 mt-4 hover:bg-[#FF5A00] active:scale-[.96] ease-out duration-100 font-poppins font-[700] tracking-wider top-[21rem] right-24 bg-[#FF7A00] rounded-3xl text-white cursor-pointer">
             LOGIN
           </button>
--- src/components/admin/UsersContent.jsx
+++ src/components/admin/UsersContent.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BlockIcon from '@mui/icons-material/Block';
+import axiosInstancce from '../../instance/axiosInstance'
 
 function UsersContent() {
+
+  const [userdata , setUserdata] = useState([])
+
+  useEffect(()=>{
+
+    const getDatas = async() =>{
+    const response = await axiosInstancce.get('/admin/users')
+    const {data , status} = response
+
+    //Saving the signupdata in data state
+    if(status == 200) 
+    setUserdata(data.signupDatas)
+    }
+
+    getDatas()
+  
+  },[])
+
+
   return (
 <div className='h-full w-full p-4 flex flex-col items-center overflow-y-scroll'>
       <div className='w-full h-auto bg-[#ffffff23] flex justify-between px-3 items-center rounded-md py-4 mb-4 sticky top-0 left-0 right-0'>
@@ -11,7 +31,6 @@ function UsersContent() {
       <table className='border border-slate-200 border-opacity-20 w-full h-auto'>
         <thead>
         <tr className='text-[.92rem] font-inter '>
-          <th className='font-roboto font-[600] border border-slate-200 border-opacity-20 py-6 min-w-32'>USERS _ID</th>
           <th className='font-roboto font-[600] border border-slate-200 border-opacity-20 py-6 min-w-32'>USER NAME</th>
           <th className='font-roboto font-[600] border border-slate-200 border-opacity-20 py-6 min-w-32'>EMAIL</th>
           <th className='font-roboto font-[600] border border-slate-200 border-opacity-20 py-6 min-w-32'>MOBILE</th>
@@ -22,18 +41,27 @@ function UsersContent() {
         </tr>
         </thead>
         <tbody>
-           <tr className='cursor-pointer'>
-            <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4'>7564856846883</td>
-            <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4'>labeeb ct</td>
-            <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4'>ctlabeebthaliyil@gmail.com</td>
+          { userdata.map((data,index) => (
+ 
+           <tr className='cursor-pointer h-[6rem]' key={index}>
+            <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4 capitalize'>{data.userName}</td>
+            <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4'>{data.email}</td>
             <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4'>8590471530</td>
             <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4'>6</td>
-            <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4'>12-04-2024</td>
-            <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4'>Active</td>
+            <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4'>
+            {new Date(data.regDate).toLocaleDateString('en-GB', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric'
+              })}
+            </td>
+            <td className={`font-[300] border border-slate-200 ${data.status == 'active' ?  'text-green-400'  : 'text-red-600'} border-opacity-20 text-center font-roboto text-[.95rem] py-4 capitalize`}>{data.status}</td>
             <td className='font-[300] border border-slate-200 border-opacity-20 text-center font-roboto text-[.95rem] py-4 text-red-600'>
               <button className='active:scale-[.9] ease-in-out duration-100'><BlockIcon /></button>
-              </td>
+            </td>
            </tr>
+ 
+           ))}
         </tbody>
       </table>
       </div>
